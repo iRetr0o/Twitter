3f5cce63a1aa6af6d6e647a736cc2a2e642ffa9a
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-04-22 18:14:15 -0500

    Paso 4: Haciendo el requerimiento 1 con TDD

---
+++ test/models/User.test.js
@@ -0,0 +1,23 @@
+const User = require("./../../app/models/User");
+
+describe("Prueba de unidad para la clase User", () => {
+  test("Creando un Objeto User", () => {
+    //Usando el código que se va usar en la app
+    const user = new User(
+      1,
+      "iretr0o",
+      "Oscar",
+      "Bio",
+      "dateCreated",
+      "lastUpdated"
+    );
+    //Aquí validamos los resultados de ese código
+    //Esta es una comparación que va igualar el valor de la izquierda con el valor de la derecha (valor esperado)
+    expect(user.id).toBe(1);
+    expect(user.username).toBe("iretr0o");
+    expect(user.name).toBe("Oscar");
+    expect(user.bio).toBe("Bio");
+    expect(user.dateCreated).toBe("dateCreated");
+    expect(user.lastUpdated).toBe("lastUpdated");
+  });
+});
