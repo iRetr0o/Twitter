4ab1d034a30e8ecb2002cacbf1ad011eaddd425e
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-04-23 16:53:43 -0500

    Paso 7: Requerimiento 4 con TDD

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -4,12 +4,11 @@ describe("Prueba de unidad para la clase User", () => {
   test("Creando un Objeto User", () => {
     //Usando el código que se va usar en la app
     const user = new User(1, "iretr0o", "Oscar", "Bio");
+    user.setUsername = "Gutierrez";
+    user.setBio = "New Bio";
     //Aquí validamos los resultados de ese código
     //Esta es una comparación que va igualar el valor de la izquierda con el valor de la derecha (valor esperado)
-    expect(user.getUsername).toBe("iretr0o");
-    expect(user.getBio).toBe("Bio");
-    //Verifica que el valor no sea undefined
-    expect(user.dateCreated).not.toBeUndefined();
-    expect(user.lastUpdated).not.toBeUndefined();
+    expect(user.username).toBe("Gutierrez");
+    expect(user.bio).toBe("New Bio");
   });
 });
