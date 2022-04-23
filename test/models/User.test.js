65c5b9d50fe8769873690950b1c8d1cc4e319579
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-04-23 15:57:33 -0500

    Paso 5: Modificando las propiedades 'dateCreated' y 'lastUpdated'

--- test/models/User.test.js
+++ test/models/User.test.js
@@ -1,6 +1,7 @@
 const User = require("./../../app/models/User");
 
 describe("Prueba de unidad para la clase User", () => {
+<<<<<<< Updated upstream
   test("Creando un Objeto User", () => {
     //Usando el código que se va usar en la app
     const user = new User(
@@ -21,3 +22,19 @@ describe("Prueba de unidad para la clase User", () => {
     expect(user.lastUpdated).toBe("lastUpdated");
   });
 });
+=======
+    test('Creando un Objeto User', () => {
+        //Usando el código que se va usar en la app
+        const user = new User(1, "iretr0o", "Oscar", "Bio");
+        //Aquí validamos los resultados de ese código
+        //Esta es una comparación que va igualar el valor de la izquierda con el valor de la derecha (valor esperado)
+        expect(user.id).toBe(1);
+        expect(user.username).toBe("iretr0o");
+        expect(user.name).toBe("Oscar");
+        expect(user.bio).toBe("Bio");
+        //Verifica que el valor no sea undefined
+        expect(user.dateCreated).not.toBeUndefined();
+        expect(user.lastUpdated).not.toBeUndefined();
+    })
+})
+>>>>>>> Stashed changes
