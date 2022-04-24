ef695ae463412f81111143c4420b8148229ed5a4
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-04-23 19:34:52 -0500

    Paso 10: Requerimiento numero 3

--- test/services/userServices.test.js
+++ test/services/userServices.test.js
@@ -1,12 +1,9 @@
 const UserService = require("./../../app/services/userServices");
 
-describe("2, Prueba de unidad para la informacion del usuario", () => {
+describe("3 acutalizando el username", () => {
   test("Creando un objeto de la clase US", () => {
     const user = UserService.create(1, "iretr0o", "Oscar");
-    const userInfoList = UserService.getInfo(user);
-    expect(userInfoList[0]).toBe(1);
-    expect(userInfoList[1]).toBe("iretr0o");
-    expect(userInfoList[2]).toBe("Oscar");
-    expect(userInfoList[3]).toBe("Sin bio");
+    UserService.updateUserUsername(user, "carlog");
+    expect(user.username).toBe("carlog");
   });
 });
