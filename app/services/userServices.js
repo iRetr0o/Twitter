4f2f344b920d25c9a14c4875ac678b189c64918e
Oscar Gutierrez <oscar_yo_123@hotmail.com>
2022-04-23 21:36:24 -0500

    Paso 13: Agregando codigo del requerimiento 4 (pruebas)

--- app/services/userServices.js
+++ app/services/userServices.js
@@ -10,6 +10,9 @@ class UserService {
   static updateUserUsername(user, newUsername) {
     return (user.username = newUsername);
   }
+  static getAllUsernames([user]) {
+    return [user.username];
+  }
 }
 
 module.exports = UserService;
